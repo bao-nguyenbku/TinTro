2d1412246ed954bfd52b8001e8ff6c537aa98d8d
Thanh Hieu <49335345+ThanhHieuDang0706@users.noreply.github.com>
2022-11-28 01:02:15 +0700

    add starter code

--- frontend/App.js
+++ frontend/App.js
@@ -1,25 +1,20 @@
-import { Text } from 'react-native';
-import { store } from './src/store';
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Provider } from 'react-redux';
-import MyApp from './src';
-import { useFonts } from 'expo-font';
+
 import * as SplashScreen from 'expo-splash-screen';
 import { NativeBaseProvider } from 'native-base';
 import { NavigationContainer } from '@react-navigation/native';
+import MyApp from './src';
+import store from './src/store';
 
 SplashScreen.preventAutoHideAsync();
 export default function App() {
   const [isReady, setIsReady] = useState(false);
-  // const [fontsLoaded] = useFonts({
-  //   'SF-Medium': require('./src/assets/fonts/SFProDisplay-Medium.ttf'),
-  // });
+
   setTimeout(() => {
-    SplashScreen.hideAsync().then(() => setIsReady(true))
+    SplashScreen.hideAsync().then(() => setIsReady(true));
   }, 2000);
-  // if (!fontsLoaded) {
-  //     return <Text>Loading...</Text>;
-  // }
+
   if (isReady) {
     return (
       <Provider store={store}>
---
+++ frontend/src/screens/logIn/index.js
@@ -0,0 +1,13 @@
+import { View } from 'react-native';
+import React from 'react';
+import { Input } from 'native-base';
+
+function LoginScreen() {
+  return (
+    <View>
+      <Input placeholder="Email" />
+    </View>
+  );
+}
+
+export default LoginScreen;
