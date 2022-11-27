05388da68de2749b9002760a70e45fea418328e1
Thanh Hieu <49335345+ThanhHieuDang0706@users.noreply.github.com>
2022-11-27 22:18:47 +0700

    add compare password

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -3,7 +3,7 @@ import { UsersService } from './../users/users.service';
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UserResponseDto } from 'src/users/dto/user.dto';
-
+import * as bcrypt from 'bcrypt';
 @Injectable()
 export class AuthService {
   constructor(
@@ -16,19 +16,21 @@ export class AuthService {
     password: string,
   ): Promise<UserResponseDto> {
     const user = await this.usersService.findByEmail(username);
-    // TODO: Hashed password using bcrypt and compare against the password in the database
     // if match return everything except the pasword
     // if not return null
-    if (user && user.password === password) {}
     if (user) {
-      
+      const { password: hashedPasswordInDb, ...result } = user;
+      const isMatchPassword = bcrypt.compare(password, hashedPasswordInDb);
+      if (!isMatchPassword) {
+        return null;
+      }
+      return result;
     }
     return null;
   }
 
   login(user: any): TokenPayload {
-    // TODO: return access token sign with jwt, naming conventions here is aligned with jwt conventions, change payload here
-    const payload = { username: user.username, sub: user.userId };
+    const payload = { username: user.email, sub: user.id };
     return {
       access_token: this.jwtService.sign(payload),
     };
