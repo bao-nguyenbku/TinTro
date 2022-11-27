b5696872eb9794d2dfc6d864e32911610fff7f0c
Thanh Hieu <49335345+ThanhHieuDang0706@users.noreply.github.com>
2022-11-28 00:07:48 +0700

    update code

--- backend/src/users/dto/create-user.dto.ts
+++ backend/src/users/dto/create-user.dto.ts
@@ -1,6 +1,5 @@
 import { UserEntity } from './../entities/user.entity';
-import { RegisterUserDtos } from './register-user.dto';
-import { PartialType, OmitType } from '@nestjs/swagger';
+import { OmitType } from '@nestjs/swagger';
 
 export class CreateUserDto extends OmitType(UserEntity, [
   'createdAt',
