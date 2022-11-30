43f46679b493bf7f2839b73ebdb409f0a66d31f7
Thanh Hieu <49335345+ThanhHieuDang0706@users.noreply.github.com>
2022-11-30 11:13:21 +0700

    delete unused  code

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,20 +1,8 @@
-import { JwtAuthGuard } from './../auth/jwt-auth.guard';
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-  UseGuards,
-} from '@nestjs/common';
+
+import { Controller, Post, Body } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
-import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
-import { Roles } from 'src/auth/role.decorator';
-import { Role } from '@prisma/client';
+import { ApiCreatedResponse, ApiTags } from '@nestjs/swagger';
 import { UserResponseDto } from './dto/user.dto';
 
 @Controller('users')
@@ -26,34 +14,5 @@ export class UsersController {
   @ApiCreatedResponse({ type: UserResponseDto })
   create(@Body() createUserDto: CreateUserDto): Promise<UserResponseDto> {
     return this.usersService.create(createUserDto);
-  }
-
-  @Get()
-  @UseGuards(JwtAuthGuard)
-  @ApiOkResponse({ type: UserResponseDto, isArray: true })
-  findAll() {
-    return this.usersService.findAll();
-  }
-
-  // protected routes
-  @UseGuards(JwtAuthGuard)
-  @Get(':id')
-  @ApiOkResponse({ type: UserResponseDto })
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
-  }
-
-  @Patch(':id')
-  @ApiOkResponse({ type: UserResponseDto })
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
-  }
-
-  @Delete(':id')
-  @UseGuards(JwtAuthGuard)
-  @ApiOkResponse({ type: UserResponseDto })
-  @Roles(Role.ADMIN)
-  remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
   }
 }
--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -3,7 +3,6 @@ import { UserEntity } from './entities/user.entity';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
@@ -36,15 +35,6 @@ export class UsersService {
       throw e;
     }
   }
-
-  findAll() {
-    return `This action returns all users`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
   // ------------------------------ find username and password --------------------------------
   async findByEmail(username: string) {
     // user name must be email
@@ -54,14 +44,5 @@ export class UsersService {
       },
     });
     return user;
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  // Example of role based on role
-  remove(id: number) {
-    return `This action removes a #${id} user`;
   }
 }
