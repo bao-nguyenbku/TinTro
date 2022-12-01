7d1df33d52deed2f8ed9a149ce9c0a7e3b84e32a
Thanh Hieu <49335345+ThanhHieuDang0706@users.noreply.github.com>
2022-12-01 15:48:54 +0700

    clean up be code + add guard

--- backend/src/accommodation/accommodation.controller.ts
+++ backend/src/accommodation/accommodation.controller.ts
@@ -1,18 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { AccommodationService } from './accommodation.service';
 
 @Controller('accommodations')
 export class AccommodationController {
   constructor(private readonly accommodationService: AccommodationService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Get()
   async getAllAccommodation() {
     try {
       const result = await this.accommodationService.getAllAccommodation();
-      return result.map(item => ({
+      return result.map((item) => ({
         ...item,
-        reviewStar: Math.round(Math.random() * 5)
-      }))
+        reviewStar: Math.round(Math.random() * 5),
+      }));
     } catch (error) {
       throw new Error(error);
     }
--- backend/src/accommodation/accommodation.service.ts
+++ backend/src/accommodation/accommodation.service.ts
@@ -4,18 +4,17 @@ import { PrismaService } from 'src/prisma/prisma.service';
 @Injectable()
 export class AccommodationService {
   constructor(private readonly prismaService: PrismaService) {}
-  
+
   getAllAccommodation() {
     return this.prismaService.accommodation.findMany({
       include: {
         owner: {
           select: {
             phone: true,
-            name: true
-          }
-        }
-      }
+            name: true,
+          },
+        },
+      },
     });
   }
-
 }
