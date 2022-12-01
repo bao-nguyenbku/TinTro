c1f4d95536204e86afbdc344a32bfbd1703fd87a
Thanh Hieu <49335345+ThanhHieuDang0706@users.noreply.github.com>
2022-12-01 12:56:59 +0700

    render authen nav condition

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,21 +4,27 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginNav from 'navigation/authentication';
 import HomeNav from 'navigation/home';
+import { useSelector } from 'react-redux';
 
 const Stack = createNativeStackNavigator();
 
 
 
-const Index = () => (
+const Index = () => {
+  const user = useSelector(state => state.user);
+  
+  return (
     <>
-
       <StatusBar/>
-      <Stack.Navigator>
-        <Stack.Screen name="Authentication" component={LoginNav} />
+      <Stack.Navigator 
+      screenOptions={{
+        headerShown: false,
+      }}>
+        {!user.currentUser && <Stack.Screen name="Authentication" component={LoginNav} /> }
         <Stack.Screen name="Home" component={HomeNav} />
       </Stack.Navigator>
     </>
 
   )
-
+}
 export default Index;
