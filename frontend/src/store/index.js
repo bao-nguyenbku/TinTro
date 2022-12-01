94a18f83ddc1334ed704e1a901d349d0c6ffc99d
Thanh Hieu <49335345+ThanhHieuDang0706@users.noreply.github.com>
2022-12-01 15:43:15 +0700

    update migrations + fix format code

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,13 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import accommodationReducer from './reducer/accommodation';
 import userReducer from './reducer/user';
-import systemReducer from './reducer/system';
 
 const store = configureStore({
   reducer: {
     user: userReducer,
     accommodation: accommodationReducer,
-    system: systemReducer,
   },
 });
 
