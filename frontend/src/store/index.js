455fd92dbeccf67055186ca455015a1a789413cf
Thanh Hieu <49335345+ThanhHieuDang0706@users.noreply.github.com>
2022-12-01 14:05:57 +0700

    fix redundant code

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -3,7 +3,7 @@ import accommodationReducer from './reducer/accommodation';
 import userReducer from './reducer/user';
 import systemReducer from './reducer/system';
 
-export const store = configureStore({
+const store = configureStore({
   reducer: {
     user: userReducer,
     accommodation: accommodationReducer,
