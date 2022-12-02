79bef2857a8c5d90661e9c36082bfcfec1a6715c
bao-nguyenbku <nguyenthienbao0402@gmail.com>
2022-12-02 19:58:52 +0700

    update schema

--- backend/src/accommodation/accommodation.controller.ts
+++ backend/src/accommodation/accommodation.controller.ts
@@ -1,11 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Roles } from 'src/auth/role.decorator';
 import { AccommodationService } from './accommodation.service';
+import { Role } from '@prisma/client';
 
 @Controller('accommodations')
 export class AccommodationController {
   constructor(private readonly accommodationService: AccommodationService) {}
 
   @Get()
+  @Roles(Role.USER)
   async getAllAccommodation() {
     try {
       const result = await this.accommodationService.getAllAccommodation();
@@ -16,5 +19,10 @@ export class AccommodationController {
     } catch (error) {
       throw new Error(error);
     }
+  }
+
+  @Post()
+  async requestRentRoom (@Body() requestRentRoomDto: RequestRentRoomDto) {
+
   }
 }
