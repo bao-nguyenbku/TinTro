498cd1af0fea87d5057fab645005f2f39b2ff4c5
DH <DH@LAPTOP-RPUH2FGN>
2022-12-07 22:21:30 +0700

    Revert "test init"
    This reverts commit ce82cd80c7e6a85274e5228d00c5d467356ed3ec.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,26 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-// import LoginNav from 'navigation/authentication';
+import LoginNav from 'navigation/authentication';
 import HomeNav from 'navigation/home';
 
-// import { useSelector } from 'react-redux';
-// import { isEmptyObj } from 'native-base';
+import { useSelector } from 'react-redux';
+import { isEmptyObj } from 'native-base';
 
 const Stack = createNativeStackNavigator();
 const Index = () => {
-  // const user = useSelector((state) => state.user);
+  const user = useSelector((state) => state.user);
 
   return (
     <>
       <StatusBar style="auto" />
-      <Stack.Navigator>
-      {/* //   screenOptions={{
-      //     headerShown: false,
-      //   }}
-      // >
-      //   {isEmptyObj(user.currentUser) && <Stack.Screen name="Authentication" component={LoginNav} />}
-      //   <Stack.Screen name="Home" component={HomeNav} /> */}
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        {isEmptyObj(user.currentUser) && <Stack.Screen name="Authentication" component={LoginNav} />}
         <Stack.Screen name="Home" component={HomeNav} />
       </Stack.Navigator>
     </>
