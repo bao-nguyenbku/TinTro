0edb6569c7e3169b725bb017628ccb613fbdfe98
Thanh Hieu <49335345+ThanhHieuDang0706@users.noreply.github.com>
2022-12-08 19:47:41 +0700

    add public folders

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,9 +6,12 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common/pipes';
 import * as morgan from 'morgan';
 import helmet from 'helmet';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import * as express from 'express';
+import path, { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {});
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {});
   app.useGlobalPipes(new ValidationPipe());
   app.use(morgan('dev'));
   const config = new DocumentBuilder()
@@ -18,6 +21,9 @@ async function bootstrap() {
     .build();
   app.use(helmet());
   app.enableCors();
+
+  app.use('/public', express.static(join(__dirname, '..', '..', 'public')));
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
   const PORT = process.env.PORT || 3001;
