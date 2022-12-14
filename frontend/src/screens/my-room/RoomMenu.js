20e21364f59c5984c09309ad48f4a19a01e78299
Thanh Hieu <49335345+ThanhHieuDang0706@users.noreply.github.com>
2022-12-14 10:02:48 +0700

    delete redundant code

--- frontend/src/screens/my-room/RoomMenu.js
+++ frontend/src/screens/my-room/RoomMenu.js
@@ -13,112 +13,83 @@ const RoomMenu = ({ navigation, stack }) => {
       id: 1,
       title: 'Gửi xe',
       description: 'Thông tin gửi xe của bạn',
-      icon: <Ionicons name='bicycle-outline' size={24}/>,
+      icon: <Ionicons name="bicycle-outline" size={24} />,
       onPress: () => navigation.navigate(stack?.parking?.title),
     },
     {
       id: 2,
       title: 'Wifi',
       description: 'Thông tin wifi phòng của bạn',
-      icon: <Ionicons name='wifi-outline' size={24}/>,
+      icon: <Ionicons name="wifi-outline" size={24} />,
       onPress: () => navigation.navigate(stack?.wifi?.title),
-    }
-  ]
+    },
+  ];
   const dispatch = useDispatch();
-  const { roomInfo, renting } = useSelector(selectRentingState);
+  const { roomInfo } = useSelector(selectRentingState);
   const { loading, data } = roomInfo;
   console.log(roomInfo);
 
   useEffect(() => {
     dispatch(getRoomInfo());
-  }, [])
+  }, []);
   if (loading) {
-    return <Loading />
+    return <Loading />;
   }
   return (
-    <ScrollView
-      refreshControl={
-        <RefreshControl 
-          refreshing={loading}
-          onRefresh={() => dispatch(getRoomInfo())}
-        />
-      }
-    >
+    <ScrollView refreshControl={<RefreshControl refreshing={loading} onRefresh={() => dispatch(getRoomInfo())} />}>
       {isEmptyObj(data) ? (
-        <Box
-        flex={1}
-        alignItems='center'
-        justifyContent='center'
-      >
-        <Text>Bạn chưa thuê phòng nào</Text>
-      </Box>
-      ): (
-        <Box
-          p='4'
-        >
-          <Box
-            alignItems='center'
-            marginBottom='8'
-          >
-            <Text fontSize='xl' fontWeight='700'>Phòng {data?.room?.roomName}</Text>
-            <Text>Tình trạng: <Text color='tertiary.600' fontWeight='700'>Đang thuê</Text></Text>
+        <Box flex={1} alignItems="center" justifyContent="center">
+          <Text>Bạn chưa thuê phòng nào</Text>
+        </Box>
+      ) : (
+        <Box p="4">
+          <Box alignItems="center" marginBottom="8">
+            <Text fontSize="xl" fontWeight="700">
+              Phòng {data?.room?.roomName}
+            </Text>
+            <Text>
+              Tình trạng:{' '}
+              <Text color="tertiary.600" fontWeight="700">
+                Đang thuê
+              </Text>
+            </Text>
             {data?.status === 'CHECKOUT' && (
-              <Box
-                bgColor='danger.100'
-                w='full'
-                rounded='xl'
-                py='8'
-                marginTop='8'
-                alignItems='center'
-              >
-                <Text fontWeight='700' fontSize='lg' color='danger.600'>Bạn đã gửi yêu cầu trả phòng</Text>
+              <Box bgColor="danger.100" w="full" rounded="xl" py="8" marginTop="8" alignItems="center">
+                <Text fontWeight="700" fontSize="lg" color="danger.600">
+                  Bạn đã gửi yêu cầu trả phòng
+                </Text>
               </Box>
             )}
           </Box>
-          <VStack
-            bgColor='white'
-            p='4'
-            marginTop='8'
-            space='4'
-            roundedTop='2xl'
-          >
-              {menus.map(menuItem => {
-                return (
-                  <TouchableOpacity 
-                    key={menuItem.id}
-                    onPress={menuItem.onPress}
-                  >
-                    <Box
-                      flexDirection='row'
-                      alignItems='center'
-                    >
-                      <Box
-                        bgColor='blueGray.200'
-                        rounded='full'
-                        w='10'
-                        h='10'
-                        alignItems='center'
-                        justifyContent='center'
-                      >
-                        {menuItem.icon}
-                      </Box>
-                      <Box marginLeft='2'>
-                        <Text fontWeight='700'>{menuItem.title}</Text>
-                        <Text color='muted.500'>{menuItem.description}</Text>
-                      </Box>
-                      <Ionicons name='chevron-forward-outline' size={24} style={{
-                        marginLeft: 'auto'
-                      }}/>
+          <VStack bgColor="white" p="4" marginTop="8" space="4" roundedTop="2xl">
+            {menus.map((menuItem) => {
+              return (
+                <TouchableOpacity key={menuItem.id} onPress={menuItem.onPress}>
+                  <Box flexDirection="row" alignItems="center">
+                    <Box bgColor="blueGray.200" rounded="full" w="10" h="10" alignItems="center" justifyContent="center">
+                      {menuItem.icon}
                     </Box>
-                  </TouchableOpacity>
-                )
-              })}
+                    <Box marginLeft="2">
+                      <Text fontWeight="700">{menuItem.title}</Text>
+                      <Text color="muted.500">{menuItem.description}</Text>
+                    </Box>
+                    <Ionicons
+                      name="chevron-forward-outline"
+                      size={24}
+                      style={{
+                        marginLeft: 'auto',
+                      }}
+                    />
+                  </Box>
+                </TouchableOpacity>
+              );
+            })}
           </VStack>
           <CheckoutButton data={data} />
         </Box>
       )}
     </ScrollView>
-  )
-}
+  );
+};
 
 export default RoomMenu;
